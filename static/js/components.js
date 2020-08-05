af7f365253bb0d565e1fc49f54812c57e3917faf
SausageMcSauce <69243388+SausageMcSauce@users.noreply.github.com>
2020-08-05 11:04:41 +0000

    Uploaded stuff. Some files might be missing or misplaced.

---
+++ static/js/components.js
@@ -0,0 +1,21 @@
+function CarmesLogo() {
+  return <a href="/carmesinc"><img src="/static/carmesinc/img/carmesinclogo.png" alt="Carmes Inc logo" /></a>;
+}
+
+function Disclaimer() {
+  return (
+    <div>
+      <p>Disclaimer: Carmes Inc is a fictional company. All products said to be by them and people said to be members do not exist in real life.</p>
+      <p>PreCure Selene was not created by me.</p>
+    </div>
+  );
+}
+
+function UnderConstruction() {
+  return (
+    <div>
+     <h2>This Page is Under Construction!</h2>
+     <p>This page is currently under construction. Parts of the page might be broken, missing or incomplete.</p>
+    </div>
+  )
+}
